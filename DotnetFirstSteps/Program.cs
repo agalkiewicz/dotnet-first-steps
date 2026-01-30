2762fe1a1ea157546f8447f62816b4b645619e86
alicja.galkiewicz <alicja.galkiewicz@gmail.com>
2026-01-30 17:05:59 +0100

    Update delete endpoint, add http file for tests

--- DotnetFirstSteps/Program.cs
+++ DotnetFirstSteps/Program.cs
@@ -1,7 +1,7 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
-app.Run();
 
 var todos = new List<Todo>();
 
@@ -19,11 +19,17 @@ app.MapPost("/todos", (Todo task) =>
     return TypedResults.Created($"/todo/{task.Id}", task);
 });
 
-app.MapDelete("/todos/{id}", Results<NoContent> (int id) =>
+app.MapDelete("/todos/{id}", (int id) =>
 {
     var task = todos.SingleOrDefault(t => t.Id == id);
+    if (task is null)
+    {
+        return TypedResults.NoContent();
+    }
     todos.Remove(task);
     return TypedResults.NoContent();
 });
+
+app.Run();
 
 public record Todo(int Id, string Name, DateTime DueDate, bool IsCompleted);
